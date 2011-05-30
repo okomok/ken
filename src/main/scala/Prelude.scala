bda3e92405250ba1bfa2df1886eec5dad9c3d97a
unknown <okomok@gmail.com>
2011-05-30 17:53:46 +0900

    Prefers currying

--- src/main/scala/Prelude.scala
+++ src/main/scala/Prelude.scala
@@ -12,29 +12,47 @@ object Prelude {
 
     def id[a](x: a): a = x
 
-    def const[a](x: a)(y: Any): a = x
+    def const[a](x: a): Any => a = { _ => x }
 
-    def apply[a, b](x: a => b)(y: a): b = x(y)
+    def compose[a, b, c](f: b => c)(g: a => b): a => c = { x => f(g(x)) }
 
-    def flip[a, b, c](x: a => b => c)(y: b)(z: a): c = x(z)(y)
+    def flip[a, b, c](f: a => b => c): b => a => c = { x => y => f(y)(x) }
 
-    def putChar(x: Char): IO[Unit] = print(x)
+    def apply[a, b](f: a => b)(x: a): b = f(x)
 
-    def putStr(x: String): IO[Unit] = print(x)
-
-    def putStrLn(x: String): IO[Unit] = new IO[Unit] {
-        override def unIO(): Unit = Predef.println(x)
+    def until[a](p: a => Boolean)(f: a => a)(x: a): a = {
+        if (p(x)) x else until(p)(f)(f(x))
     }
 
-    def print(x: Any): IO[Unit] = new IO[Unit] {
-        override def unIO(): Unit = Predef.print(x)
+    def asTypeOf[a](x: a): a => a = const(x)
+
+    def error(msg: String): Nothing = throw new Error(msg)
+
+    def undefined: Nothing = throw new Error
+
+    val show: Any => String = { x => x.toString }
+
+    val putChar: Char => IO[Unit] = print(_)
+
+    val putStr: String => IO[Unit] = print(_)
+
+    val putStrLn: String => IO[Unit] = { x =>
+        new IO[Unit] {
+            override def unIO(): Unit = Predef.println(x)
+        }
     }
 
-    def getChar: IO[Char] = new IO[Char] {
+    val print: Any => IO[Unit] = { x =>
+        new IO[Unit] {
+            override def unIO(): Unit = Predef.print(x)
+        }
+    }
+
+    val getChar: IO[Char] = new IO[Char] {
         override def unIO(): Char = Predef.readChar()
     }
 
-    def getLine: IO[String] = new IO[String] {
+    val getLine: IO[String] = new IO[String] {
         override def unIO(): String = Predef.readLine()
     }
 
