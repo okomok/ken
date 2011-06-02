c56d71818166a89e929bc6cef07e015f4d23b87a
unknown <okomok@gmail.com>
2011-06-02 20:42:45 +0900

    MonadPlus operations

--- src/main/scala/MonadPlus.scala
+++ src/main/scala/MonadPlus.scala
@@ -8,12 +8,22 @@ package com.github.okomok
 package ken
 
 
-trait MonadPlus[f[_]] extends Monad[f] {
-    def mzero[a]: f[a]
-    def mplus[a](x: f[a])(y: f[a]): f[a]
+import Prelude._
 
-    private[ken] class _Mplus_[a](x: f[a]) {
-        def _mplus_(y: f[a]): f[a] = mplus(x)(y)
+
+trait MonadPlus[m[_]] extends Monad[m] {
+    def mzero[a]: m[a]
+    def mplus[a](x: m[a])(y: m[a]): m[a]
+
+    private[ken] class _Mplus_[a](x: m[a]) {
+        def _mplus_(y: m[a]): m[a] = mplus(x)(y)
     }
-    implicit def _mplus_[a](x: f[a]): _Mplus_[a] = new _Mplus_(x)
+    implicit def _mplus_[a](x: m[a]): _Mplus_[a] = new _Mplus_(x)
+
+    final def guard(b: Boolean): m[Unit] = b match {
+        case true => `return`(())
+        case false => mzero
+    }
+
+    final def msum[a](xs: List[m[a]]): m[a] = foldr(Lazy.r(mplus[a]))(mzero)(xs)
 }
