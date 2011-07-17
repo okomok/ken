c03c0fb4f084fc2128a9a97a274138321ea9a96f
unknown <okomok@gmail.com>
2011-07-17 11:05:03 +0900

    nomsg

--- src/main/scala/MonadFix.scala
+++ src/main/scala/MonadFix.scala
@@ -12,7 +12,13 @@ trait MonadFix[m[+_]] extends Monad[m] {
     def mfix[a](f: (=> a) => m[a]): m[a]
 }
 
+
 trait MonadFixProxy[m[+_]] extends MonadFix[m] with MonadProxy[m] {
     override def self: MonadFix[m]
     override def mfix[a](f: (=> a) => m[a]): m[a] = self.mfix(f)
+}
+
+
+object MonadFix {
+    def apply[m[+_]](implicit i: MonadFix[m]): MonadFix[m] = i
 }
