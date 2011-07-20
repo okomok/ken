520f684698b21e4d934d6de196d6f83339da9f18
unknown <okomok@gmail.com>
2011-07-20 22:04:48 +0900

    ErrorT example

--- src/main/scala/Either.scala
+++ src/main/scala/Either.scala
@@ -13,11 +13,38 @@ sealed abstract class Either[+a, +b] extends Up[Either[a, b]] {
     final def of[a_ >: a, b_ >: b]: Either[a_, b_] = this
 }
 
-final case class Left[+a, +b](x: a) extends Either[a, b]
-final case class Right[+a, +b](y: b) extends Either[a, b]
+final case class Left[+a](x: a) extends Either[a, Nothing]
+final case class Right[+b](y: b) extends Either[Nothing, b]
 
 
-object Either {
+trait EitherLowPriorityImplicits { this: Either.type =>
+    implicit def monad[e]: MonadFix[({type m[+a] = Either[e, a]})#m] = new MonadFix[({type m[+a] = Either[e, a]})#m] {
+        // Functor
+        private[this] type f[+a] = Either[e, a]
+        override def fmap[a, b](f: a => b)(e: f[a]): f[b] = e match {
+            case Left(l) => Left(l)
+            case Right(r) => Right(f(r))
+        }
+        // Monad
+        private[this] type m[+a] = f[a]
+        override def `return`[a](x: a): m[a] = Right(x)
+        override def op_>>=[a, b](m: m[a])(k: a => m[b]): m[b] = m match {
+            case Left(l) => Left(l)
+            case Right(r) => k(r)
+        }
+        // MonadFix
+        override def mfix[a](f: (=> a) => m[a]): m[a] = {
+            lazy val a: m[a] = f { a match {
+                case Right(r) => r
+                case _ => error("empty mfix argument")
+            } }
+            a
+        }
+    }
+}
+
+
+object Either extends EitherLowPriorityImplicits {
     def either[a, b, c](f: a => c)(g: b => c)(e: Either[a, b]): c = e match {
         case Left(x) => f(x)
         case Right(y) => g(y)
@@ -32,11 +59,25 @@ object Either {
         List.foldr[Either[a, b], (List[a], List[b])](either(left)(right))((Nil, Nil))(x)
     }
 
-    implicit def functor[A]: Functor[({type f[+x] = Either[A, x]})#f] = new Functor[({type f[+x] = Either[A, x]})#f] {
-        private[this] type f[x] = Either[A, x]
-        override def fmap[a, b](f: a => b)(e: f[a]): f[b] = e match {
-            case Left(x) => Left(x)
-            case Right(y) => Right(f(y))
+    /*
+    implicit def monadError[e](implicit i: ErrorClass[e]): MonadPlus[({type m[+a] = Either[e, a]})#m] with MonadError[e, ({type m[+a] = Either[e, a]})#m]
+        = new MonadPlus[({type m[+a] = Either[e, a]})#m] with MonadError[e, ({type m[+a] = Either[e, a]})#m] with MonadProxy[({type m[+a] = Either[e, a]})#m]
+    {
+        override val self = monad[e]
+        // MonadPlus
+        private[this] type m[+a] = Either[e, a]
+        override def mzero: m[Nothing] = Left(i.noMsg)
+        override def mplus[a](m: m[a])(n: => m[a]): m[a] = m match {
+            case Left(_) => n
+            case _ => m
+        }
+        // MonadError
+        override def errorClass: ErrorClass[e] = i
+        override def throwError[a](e: e): m[a] = Left(e)
+        override def catchError[a](m: m[a])(h: e => m[a]): m[a] = m match {
+            case Left(l) => h(l)
+            case Right(r) => Right(r)
         }
     }
+    */
 }
---
+++ src/test/scala/example/ParseError.scala
@@ -0,0 +1,67 @@
+
+
+// Copyright Shunsuke Sogame 2011.
+// Distributed under the terms of an MIT-style license.
+
+
+package com.github.okomok.kentest.example
+
+
+import com.github.okomok.ken._
+
+
+// From: http://monads.haskell.cz/html/errormonad.html
+
+
+class ParseErrorTest extends org.scalatest.junit.JUnit3Suite {
+
+    final class ParseError(val location: Int, val reason: String_)
+
+    object Err {
+        def apply(l: Int)(r: String_): ParseError = new ParseError(l, r)
+    }
+
+    implicit object ParseErrorClass extends ErrorClass[ParseError] {
+        override def noMsg = Err(0)("ParseError")
+        override def strMsg = s => Err(0)(s)
+    }
+
+    implicit val ParseMonad = Error.monad[ParseError] // Monad[({type m[+a] = ErrorT[ParseError, a]})#m]
+    type ParseMonad[+a] = ParseMonad.apply[a] // ErrorT[ParseError, a]
+
+    import ParseMonad._
+
+    def isHexDigit: Char => Boolean = c => true
+
+    def parseHexDigit: Char => Int => ParseMonad[Int] = c => idx => {
+        if (isHexDigit(c)) {
+            `return`(c.toInt)
+        } else {
+            throwError(Err(idx)("Invalid character " ::: List(c)))
+        }
+    }
+
+    def parseHex: String_ => ParseMonad[Int] = { s =>
+        def parseHex_ : String_ => Int => Int => ParseMonad[Int] = cs => v => idx => cs match {
+            case Nil =>`return`(v)
+            case c :: cs => for {
+                d <- parseHexDigit(c)(idx)
+                * <- parseHex_(cs.!)(v * 16 + d)(idx + 1)
+            } yield *
+        }
+        parseHex_(s)(0)(1)
+    }
+
+    def toString_ : Int => ParseMonad[String_] = n => `return` { show(n) }
+
+    def convert: String_ => String_ = s => {
+        val Right(str) = catchError( for { n <- parseHex(s); * <- toString_(n) } yield * ) { e =>
+            `return` { "At Index " ::: show(e.location) ::: ":" ::: e.reason }
+        } run
+
+        str
+    }
+
+    def test_ {
+    }
+}
