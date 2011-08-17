7cf52c1bf5e8096ad07b87d4ebedecb6b080cef4
unknown <okomok@gmail.com>
2011-08-18 08:40:37 +0900

    More LazyTests.

--- src/test/scala/com/github/okomok/kentest/LazyTest.scala
+++ src/test/scala/com/github/okomok/kentest/LazyTest.scala
@@ -12,14 +12,27 @@ import com.github.okomok.ken._
 
 class LazyTest extends org.scalatest.junit.JUnit3Suite {
 
+    def testLazy {
+        Lazy(throw new java.lang.Error("wow"))
+    }
+
     def testEval {
         class HasBang {
             def ! : String = "hello"
+        }
+
+        class HasEval {
+            var called = false
+            def _eval: HasEval = { called = true; this }
         }
 
         object NoBang
 
         expect("hello")(Lazy(new HasBang).!)
         expect(NoBang)(Lazy(NoBang).!)
+
+        val he = new HasEval
+        Lazy(he)._eval
+        assert(he.called)
     }
 }
