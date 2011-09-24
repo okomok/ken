8ff563ed064d6697f35281a696b8c598b7130ce3
unknown <okomok@gmail.com>
2011-09-25 04:00:20 +0900

    more tests

--- src/test/scala/com/github/okomok/kentest/Newtype0Test.scala
+++ src/test/scala/com/github/okomok/kentest/Newtype0Test.scala
@@ -15,7 +15,7 @@ class Newtype0Test extends org.scalatest.junit.JUnit3Suite {
     final case class Wrap[a](override val get: a) extends NewtypeOf[a]
 
     object Wrap {
-        implicit def _asNewType0[a]: Newtype0[Wrap[a], a, Num :^: Ord :^: Real :^: Enum :^: Integral :^: Floating :^: Kind.nil] = new Newtype0[Wrap[a], a, Num :^: Ord :^: Real :^: Enum :^: Integral :^: Floating :^: Kind.nil] {
+        implicit def _asNewType0[a]: Newtype0[Wrap[a], a, Num :^: Ord :^: Real :^: Enum :^: Floating :^: Kind.nil] = new Newtype0[Wrap[a], a, Num :^: Ord :^: Real :^: Enum :^: Floating :^: Kind.nil] {
             override val newOf: newOf = ot => Wrap(ot)
             override val oldOf: oldOf = nt => nt.get
         }
@@ -24,11 +24,14 @@ class Newtype0Test extends org.scalatest.junit.JUnit3Suite {
     def testTrivial {
         Num[Wrap[Int]]
         Ord[Wrap[Int]]
-        Integral[Wrap[Int]]
         Enum[Wrap[Int]]
         Real[Wrap[Int]]
 
+        Integral[Kind.const[Int]] // but,...
+        // Integral[Wrap[Int]] // must be error.
+
         // Floating[Wrap[Int]] // must be error.
+
     }
 
 }
